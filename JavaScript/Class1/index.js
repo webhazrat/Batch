0980d1b1671ce69bfd4b3d0046bac5e6a8358dbd
Md Hazrat <webhazrat>
2023-06-20 23:31:50 +0600

    Javascript Init

---
+++ JavaScript/Class1/index.js
@@ -0,0 +1,33 @@
+// variable identifier
+// data type - number, string, boolean, object, array
+
+// number
+var a = 12;
+
+// string
+var b = "jsdkjfkdjfk";
+
+// boolean
+var c = true;
+
+// object
+var abc = {
+  a: 12,
+  b: "hsjdhfja",
+  c: 10,
+  d: {
+    a: "da",
+  },
+  f: function () {
+    return "F Function";
+  },
+};
+// abc.a
+
+// array
+var abcArray = [12, "hsjdhfjsdh", 10, "hsjdfhjdsh"];
+// abcArray[0]
+
+abc.a = 100;
+
+console.log(abc);
