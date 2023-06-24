e1a7891ff9ee9ea30db7ade790171103569c201a
Md Hazrat <webhazrat>
2023-06-24 11:08:00 +0600

    JS update

--- Projects/HooBank/assets/js/index.js
+++ Projects/HooBank/assets/js/index.js
@@ -18,4 +18,42 @@ const testimonial = new Swiper(".testimonial-swiper", {
     el: ".testimonial-swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+    },
+    767: {
+      slidesPerView: 2,
+    },
+    557: {
+      slidesPerView: 1,
+    },
+  },
 });
+
+// branching (if, switch), looping
+
+var day = "Sunday";
+
+if (
+  day === "Sunday" ||
+  day === "Monday" ||
+  day === "Tuesday" ||
+  day === "Wednesday" ||
+  day === "Thursday"
+) {
+  console.log("Working Day");
+} else if (day === "Saturday" || day === "Friday") {
+  console.log("Holi Day");
+} else {
+  console.log("You have added wrong");
+}
+
+var nums = [12, 10, 78, 8, 10, 62, 22];
+
+// for(initial; limit; increment/decrement)
+var total = 0;
+for (var i = 0; i < nums.length; i++) {
+  total = total + nums[i];
+}
+console.log(total);
